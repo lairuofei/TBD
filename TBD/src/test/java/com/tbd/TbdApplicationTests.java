2cd1e7dae1231b43278d72319e7110aa9658a80a
Ruofei <523184734@qq.com>
2022-02-25 23:23:05 +0800

    # init

--- TBD/src/test/java/com/tbd/TbdApplicationTests.java
+++ TBD/src/test/java/com/tbd/TbdApplicationTests.java
@@ -1,13 +1,16 @@
 package com.tbd;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class TbdApplicationTests {
+public class TbdApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
