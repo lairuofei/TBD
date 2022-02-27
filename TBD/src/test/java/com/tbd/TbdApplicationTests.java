4afedbb5ed0f5ae141f850b139e987c8ba60b0d6
Ruofei <523184734@qq.com>
2022-02-27 18:25:24 +0800

    # init

--- TBD/src/test/java/com/tbd/TbdApplicationTests.java
+++ TBD/src/test/java/com/tbd/TbdApplicationTests.java
@@ -1,16 +1,29 @@
 package com.tbd;
 
+import com.tbd.dao.TestTableDao;
+import com.tbd.domain.TestTable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.Serializable;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TbdApplicationTests {
+public class TbdApplicationTests implements Serializable {
+
+
+    @Autowired
+    TestTableDao testTableDao;
 
     @Test
     public void contextLoads() {
+        testTableDao.insert(new TestTable()
+                .setId(1L)
+                .setName("ruofei"));
     }
 
 }
